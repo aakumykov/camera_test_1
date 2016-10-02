1e7a8aa2945ed54f11aba5cc98c115da88a6f782
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-02 23:54:39 +0700

    Таблица на главной странице

--- www/js/index.js
+++ www/js/index.js
@@ -17,59 +17,59 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+	// Application Constructor
+	initialize: function() {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
 
-        document.getElementById('photo_button').addEventListener('click', this.takeAPhoto, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+		document.getElementById('photo_button').addEventListener('click', this.takeAPhoto, false);
+	},
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicitly call 'app.receivedEvent(...);'
+	onDeviceReady: function() {
+		
+	},
+	//
+	displayInfo: function(msg) {
+		document.getElementById('info').innerHTML = String(msg);
+	},
+	// Фотокамера
+	takeAPhoto: function(){
+		navigator.camera.getPicture(
+			onSuccess, 
+			onFail, 
+			{ 
+				quality: 50, 
+				sourceType: Camera.PictureSourceType.CAMERA,
+				//sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+				
+				destinationType: Camera.DestinationType.DATA_URL,
+				//destinationType: Camera.DestinationType.FILE_URI,
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+				allowEdit: true,
+				encodingType: Camera.EncodingType.PNG,
+			}
+		);
 
-        console.log('Received Event: ' + id);
-    },
-    // Фотокамера
-    takeAPhoto: function(){
-        navigator.camera.getPicture(
-            onSuccess, 
-            onFail, 
-            { quality: 50, destinationType: Camera.DestinationType.FILE_URI }
-        );
+		function onSuccess(imageData) {
+			app.displayInfo(typeof(imageData)+', '+imageData.length);
 
-        function onSuccess(imageData) {
-            //alert(imageData);
-            var image = document.getElementById('photo_picture');
-                image.src = "data:image/jpeg;base64,"+imageData;
-            navigator.camera.cleanup(
-                function(){},
-                function(msg){ alert('Ошибка очистки фотокамеры: '+msg)}
-            );
-        }
+			var image = document.getElementById('photo_picture');
+			image.src = "data:image/jpeg;base64,"+imageData;
+		}
 
-        function onFail(message) {
-            alert('Failed because: ' + message);
-        }
-    }
+		function onFail(message) {
+			alert('Failed because: ' + message);
+		}
+	}
 };
 
 app.initialize();
